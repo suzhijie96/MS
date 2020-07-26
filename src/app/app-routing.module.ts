36d1d126dc8eb2f8f92e794eacd0139755762069
suzhijie96 <48274366+suzhijie96@users.noreply.github.com>
2020-07-26 22:10:56 +0800

    修改路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component'
+import {ResetPasswordComponent} from './reset-password/reset-password.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'Index', component: DashboardComponent },
-  { path: 'AddCamera', component: AddCameraComponent},
-  { path: 'Live', component: LiveComponent}
+  { path: 'Index', component: DashboardComponent,
+    children: [
+      { path: 'AddCamera', component: AddCameraComponent},
+      { path: 'Live', component: LiveComponent},
+  ]},
+
+  { path: 'resetPassword', component: ResetPasswordComponent },
+
 ]
 
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router} from '@angular/router'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -24,13 +25,21 @@ export class LoginComponent implements OnInit {
 
   submitForm(): void {
     // tslint:disable-next-line: forin
+    console.log("userName: " + this.validateForm.controls.userName.value);
+    console.log("password: " + this.validateForm.controls.password.value);
     for (const i in this.validateForm.controls) {
       this.validateForm.controls[i].markAsDirty();
       this.validateForm.controls[i].updateValueAndValidity();
     }
   }
 
-  constructor(private fb: FormBuilder) {}
+  //跳转到修改密码页面
+  GoToResetPwd() {
+    let validate = this.validateForm.controls.userName;
+    console.log(validate);
+    this.router.navigate(['/resetPassword']);
+  }
+  constructor(public router:Router, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
